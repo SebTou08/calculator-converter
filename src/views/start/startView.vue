34089d80f4c589377d7c75c937cd98235e663abb
Sebastian Toulier <sebastian.tf@appeiron.tech>
2022-04-02 11:02:37 -0500

    first commit

---
+++ src/views/start/startView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="fullPageBackgrounf">
+    <button></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "startView",
+};
+</script>
+
+<style scoped>
+.fullPageBackgrounf {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: linear-gradient(135deg, #43c6ac, #191654, #572692);
+}
+</style>
